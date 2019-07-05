e7ef9de8c6dba373d8ce51712b9535ad136b2f92
Michael Grudkowski <mgrudkowski+123@gmail.com>
2019-07-05 14:34:14 -0400

    Comment on insertion sort

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -10,11 +10,15 @@ def insertionSort(ar):
       ar[j] = ar[j - 1]
       j -= 1
 
+    # if the item to the right of the sorted array(ar[i]) was already in the correct position, this
+    # will be a no-op
     ar[j] = v
 
 
 if __name__ == "__main__":
-  ar = [43,46,26,7,63,451,65,457,658,3,76,35,1,54,65,8,745,1361,7]
+  #ar = [43,46,26,7,63,451,65,457,658,3,76,35,1,54,65,8,745,1361,7]
+  #ar = [2,3,4,5,6,7,8,9,10,1]
+  ar = [1,2,3,4,5,6,7,8,9,10, 1]
   print(ar)
   insertionSort(ar)
   print(ar)
